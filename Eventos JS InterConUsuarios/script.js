124aff614002ec3f104256e0bb5848b8d76bc47c
Sergio Santa <sergiosanta970617@gmail.com>
2022-09-10 12:15:59 -0500

    Calculadora suma, resta, div, multi

--- Eventos JS InterConUsuarios/script.js
+++ Eventos JS InterConUsuarios/script.js
@@ -1,11 +1,34 @@
-const PrimerCalculo = document.querySelector('#calculo1');
-const SegundoCalculo = document.getElementById('calculo2');
-const producto = document.getElementById ('resultado');
+const sum1 = document.getElementById ('suma1');
+const sum2 = document.getElementById ('suma2');
+const rest1 = document.getElementById ('resta1');
+const rest2 = document.getElementById ('resta2');
+const div1 = document.getElementById ('division1');
+const div2 = document.getElementById ('division2');
+const multi1 = document.getElementById ('multiplicacion1');
+const multi2 = document.getElementById ('multiplicacion2');
+
+const ResultSuma = document.getElementById ('ProductoSuma'); 
+const ResultResta = document.getElementById ('ProductoResta'); 
+const ResultDivision = document.getElementById ('ProductoDivision'); 
+const ResultMultiplicacion = document.getElementById ('Productomultiplicador'); 
 
 
+function SumandoValores () {
+    operacionsuma = parseInt(sum1.value) + parseInt(sum2.value);
+    ResultSuma.innerHTML = "La suma es igual a " + operacionsuma; 
+}
 
-function SumarProductos () {
-    
-    const Sumafinal = parseInt(PrimerCalculo.value) + parseInt(SegundoCalculo.value);
-    producto.innerHTML = 'El resultado es' + ' ' + Sumafinal;
+function RestaValores () {
+    operacionresta = parseInt(rest1.value) - parseInt(rest2.value);
+    ResultResta.innerHTML = "La resta es igual a " + operacionresta; 
+}
+
+function DivisionValores () {
+    operaciondivision = parseInt(div1.value) / parseInt(div2.value);
+    ResultDivision.innerHTML = "La división es igual a " + operaciondivision; 
+}
+
+function MultiplicadorValores () {
+    operacionmultiplicador = parseInt(multi1.value) * parseInt(multi2.value);
+    ResultMultiplicacion.innerHTML = "La multiplicación es igual a " + operacionmultiplicador; 
 }
